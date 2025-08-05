42e11c694eea46ba693b3316bea31503304c8ff9
Qingyu Wang <40660121+colinaaa@users.noreply.github.com>
2025-08-05 20:37:24 +0800

    Init repro

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from "@rsbuild/core";
+import type { RsbuildPlugin } from "@rsbuild/core";
 
 export default defineConfig({
   output: {
@@ -6,4 +7,38 @@ export default defineConfig({
       root: "./rsbuild-dist",
     },
   },
+  plugins: [
+    {
+      name: "foo",
+      setup(api) {
+        api.modifyRspackConfig((config) => {
+          return {
+            ...config,
+            module: {
+              ...config.module,
+              rules: [
+                ...(config.module?.rules || []),
+                {
+                  test: /\.foo/,
+                  use: "foo-loader",
+                },
+              ],
+            },
+          };
+        });
+      },
+    } satisfies RsbuildPlugin,
+    {
+      name: "bar",
+      setup(api) {
+        api.modifyRspackConfig((config, { appendRules }) => {
+          appendRules({
+            test: /\.bar/,
+            use: "bar-loader",
+          });
+          return config;
+        });
+      },
+    } satisfies RsbuildPlugin,
+  ],
 });
